6192e512f752241770cb2069a82db8ddeb5d5f22
joelwkkr <126818741+joelwkkr@users.noreply.github.com>
2023-06-15 12:29:25 -0500

    Added files for main, file, and webpage

---
+++ src/lib/file.py
@@ -0,0 +1,19 @@
+import pandas
+
+
+class File:
+
+    def __init__(self, name, file_type):
+        self.name = name
+        self.file_type = file_type
+
+        return
+
+    def open(self):
+        return
+
+    def close(self):
+        return
+
+    def retrieve_value(self, name):
+        return
---
+++ src/lib/webpage.py
@@ -0,0 +1,19 @@
+import selenium
+
+
+class Webpage:
+
+    def __init__(self):
+        return
+
+    def click(self, field):
+        return
+
+    def enter_value(self, value, field):
+        return
+
+    def close(self):
+        return
+
+    def open(self):
+        return
